2e211ab43bd4451c69b5360a020d9bc9b7e9fd6d
Edison <edison.r.cuya.alviz@gmail.com>
2024-05-23 16:22:28 -0500

    meinioru2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Paragon Ecoomerce",
-  description: "Paragon Ecoomerce",
+  description: "Paragon Ecoomerce mein ioru",
 };
 
 export default function RootLayout({
