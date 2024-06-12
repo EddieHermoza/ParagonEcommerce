cc868de6ccde28d0569e575921b6a86dd1e17359
Edison <edison.r.cuya.alviz@gmail.com>
2024-06-12 15:09:47 -0500

    add hook media query to custom banner component

--- src/app/(shop)/about/page.tsx
+++ src/app/(shop)/about/page.tsx
@@ -3,7 +3,6 @@ import {CustomBanner} from "@/components/ui"
 export default function Page() {
   return (
     <div>
-      <h1>Hello Page</h1>
       <CustomBanner banner="/images/MSI.webp" bannerMobile="/images/MSI_Mobile.webp" />
     </div>
   );
--- src/components/ui/CustomBanner.tsx
+++ src/components/ui/CustomBanner.tsx
@@ -1,27 +1,35 @@
+'use client'
+import Image from "next/image";
+import { useMediaQuery } from "@/app/hooks/useMediaQuery";
 
-import Image from 'next/image';
 type Props = {
   banner: string;
-  bannerMobile:string
-}
-
-
-
-
+  bannerMobile: string;
+};
 
 export default function CustomBanner(props: Props) {
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
-  return(
-
-    <section className='w-screen h-auto relative filter saturate-150 overflow-y-hidden'>
-
-      <Image className='parallax-animation w-full max-md:hidden bg-gray-900 ' src={props.banner} width={2000} height={714} alt=''></Image>
-
-      <Image className='parallax-animation w-full md:hidden bg-gray-900' src={props.bannerMobile} width={1200} height={1600} alt=''></Image>
-
+  return (
+    <section className="w-screen h-auto relative filter saturate-150 overflow-y-hidden">
+      {isMobile ? (
+        <Image
+          className="parallax-animation w-full bg-gray-900"
+          src={props.bannerMobile}
+          width={1200}
+          height={1600}
+          alt="Mobile Banner"
+        />
+      ) : (
+        <Image
+          className="parallax-animation w-full bg-gray-900"
+          src={props.banner}
+          width={2000}
+          height={714}
+          alt="Desktop Banner"
+        />
+      )}
       <div className="absolute inset-x-0 bottom-0 h-24 bg-gradient-to-t from-black to-transparent opacity-70"></div>
-
-    </section> 
-
-  )
+    </section>
+  );
 }
