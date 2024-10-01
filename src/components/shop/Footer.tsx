1a36371b920c40f33cadd0d2b75fab2a3ea2686b
EddieHermoza <eddie.ehc04@gmail.com>
2024-09-30 23:21:21 -0500

    fix : footer

--- src/components/shop/Footer.tsx
+++ src/components/shop/Footer.tsx
@@ -1,5 +1,5 @@
 import { titilium } from "@/app/config/font";
-export default function Footer() {
+export function Footer() {
   return (
     <footer className="h-[70px] flex items-center justify-center max-sm:text-xs sm:text-base text-center w-full px-3 bg-secondary">
         <p className={titilium.className}> © 2023 © Paragon.  Todos los derechos reservados. </p>
